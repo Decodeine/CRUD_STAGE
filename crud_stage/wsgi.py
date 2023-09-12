da508567502eb6abd2267f4e159905423c023a13
Decodeine <abionaadesola@yahoo.com>
2023-09-12 06:05:14 +0100

    wsgi

--- crud_stage/wsgi.py
+++ crud_stage/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUD__operation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crud__OPERATION.settings')
 
 application = get_wsgi_application()
