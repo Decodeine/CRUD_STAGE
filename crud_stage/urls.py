595dded0a1baa84b5cb65a4735bf141d904b6605
Decodeine <abionaadesola@yahoo.com>
2023-09-12 09:42:10 +0100

    url update

--- crud_stage/urls.py
+++ crud_stage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PersonCreateView, PersonRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('', PersonCreateView.as_view(), name='person-list-create'),
+    path('api', PersonCreateView.as_view(), name='person-list-create'),
     path('<str:pk_or_name>/', PersonRetrieveUpdateDeleteView.as_view(), name='person-detail'),
 ]
